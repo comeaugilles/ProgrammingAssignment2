27b54a6e7446bdb2cade70fa47bda8ddd3f5b63d
comeaugilles <comeaugilles@hotmail.com>
2015-07-22 23:16:05 -0300

    commiting Solution

--- cachematrix.R
+++ cachematrix.R
@@ -1,15 +1,46 @@
-## Put comments here that give an overall description of what your
-## functions do
+## This function creates a matrix that is able to cache it's inverse.
+## It seems to be making an object like an object oriented language
 
-## Write a short comment describing this function
+makeCacheMatrix <- function(x = matrix()) { 
+        
+        inversedMatrix <- NULL 
+        
+        set <- function(y) { 
+                x <<- y 
+                inversedMatrix <<- NULL 
+        } 
+        
+        get <- function() x 
+        
+        setInversedMatrix <- function(inverse) inversedMatrix <<- inverse 
+        
+        getInversedMatrix <- function() inversedMatrix 
+        
+        list(set=set, get=get, setInversedMatrix=setInversedMatrix, getInversedMatrix=getInversedMatrix) 
+} 
 
-makeCacheMatrix <- function(x = matrix()) {
-
-}
 
 
-## Write a short comment describing this function
+## This function computes the inverse of the matrix object created in the first function 
+## called makeCacheMatrix.  If the inverse has already been calculated (and the matrix 
+## has not changed), then the cachesolve should retrieve the inverse from the cache.
 
-cacheSolve <- function(x, ...) {
-        ## Return a matrix that is the inverse of 'x'
+cacheSolve <- function(x, ...) { 
+        
+        inversedMatrix <- x$getInversedMatrix() 
+        
+        if(!is.null(inversedMatrix)) { 
+                message("getting cached data.") 
+                return(inversedMatrix) 
+        } 
+        
+        data <- x$get() 
+        
+        # Documentation for Solve can be found here: http://www.statmethods.net/advstats/matrix.html
+        # solve computes the inverse of the matrix.
+        inversedMatrix <- solve(data) 
+        
+        x$setInversedMatrix(inversedMatrix) 
+        
+        inversedMatrix 
 }
